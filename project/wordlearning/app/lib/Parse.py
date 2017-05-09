d857069791fcc04408b242e940c10f71d7aaddde
m-tkg <msk.takagi@gmail.com>
2017-05-09 14:53:17 +0900

    fix regex

--- project/wordlearning/app/lib/Parse.py
+++ project/wordlearning/app/lib/Parse.py
@@ -18,11 +18,8 @@ class Parse:
         readable_article = BeautifulSoup(Document(html).summary().replace('</p>', '\n</p>'), 'lxml').getText()
         readable_title = Document(html).short_title()
         all_text = dt.now().strftime('%Y-%m-%d') + '\n' + readable_title + '\n' + readable_article
-
-        all_text = re.sub(r'\r\n', '\n', all_text, flags=re.MULTILINE)
-        all_text = re.sub(r'\n+', '\n', all_text, flags=re.MULTILINE)
-        all_text = re.sub(r'\t+', ' ', all_text, flags=re.MULTILINE)
-        all_text = re.sub(r' +', ' ', all_text, flags=re.MULTILINE)
+        all_text = re.sub(r'[\r\n]+', '\n', all_text, flags=re.MULTILINE)
+        all_text = re.sub(r'[\t ]+', ' ', all_text)
         all_text = re.sub(r'^ ', '', all_text)
         all_text = re.sub(r' $', '', all_text)
         return all_text
