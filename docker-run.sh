f44ea6fca0d7a57ab8798b07309a047723a07218
m-tkg <msk.takagi@gmail.com>
2017-05-10 16:50:30 +0900

    optimize current dir

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 ROOTDIR=$(cd $(dirname $0) && pwd)
-test ! -r $ROOTDIR/config.file && exit 200
-. $ROOTDIR/config.file
+test ! -r ${ROOTDIR}/config.file && exit 200
+. ${ROOTDIR}/config.file
 
-cd $ROOTDIR
+cd ${ROOTDIR}
 
 SUFFIX=run
 OPTION=""
@@ -12,4 +12,4 @@ echo launch local image.
 IMAGE=${IMAGE_LOCAL}
 
 docker ps -a | grep -q ${IMAGE_NAME}-${SUFFIX} && docker rm -f ${IMAGE_NAME}-${SUFFIX} || true
-docker run --cap-add=ALL -p 8080:80 -v ${HOME}/work/docker/english/project:/root/project -d -P -h ${IMAGE_NAME}-${SUFFIX} --name ${IMAGE_NAME}-${SUFFIX} ${IMAGE}
+docker run --cap-add=ALL -p 8080:80 -v ${ROOTDIR}/project:/root/project -d -P -h ${IMAGE_NAME}-${SUFFIX} --name ${IMAGE_NAME}-${SUFFIX} ${IMAGE}
