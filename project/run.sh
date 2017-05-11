be3c8dd1b3b3e5324e36869ae3c97e583725a760
m-tkg <msk.takagi@gmail.com>
2017-05-11 11:01:03 +0900

    wait until db starts

--- project/run.sh
+++ project/run.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
 ROOTDIR=$(cd $(dirname $0) && pwd)
-python ${ROOTDIR}/wordlearning/manage.py makemigrations
-python ${ROOTDIR}/wordlearning/manage.py migrate
-python ${ROOTDIR}/wordlearning/manage.py runserver 0.0.0.0:8080
+LOGFILE=${ROOTDIR}/wordlearning.log
+MAX_RETRY=5
+
+retry() {
+  n=0
+  until [ $n -ge ${MAX_RETRY} ]
+  do
+    "${@}" >>${LOGFILE} 2>&1 && break
+    n=$[${n}+1]
+    sleep 1
+  done
+  if [ $n -ge ${MAX_RETRY} ]; then
+    echo "failed: ${@}" >>${LOGFILE} 2>&1
+    exit 1
+  fi
+}
+
+touch ${LOGFILE}
+retry python ${ROOTDIR}/wordlearning/manage.py makemigrations
+retry python ${ROOTDIR}/wordlearning/manage.py migrate
+retry python ${ROOTDIR}/wordlearning/manage.py runserver 0.0.0.0:8080
