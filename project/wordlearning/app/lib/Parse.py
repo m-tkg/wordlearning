cb6f0d80ff44ed2bfd280dbcf0b16f4abab5ca16
m-tkg <msk.takagi@gmail.com>
2017-05-15 12:12:37 +0900

    add user agent

--- project/wordlearning/app/lib/Parse.py
+++ project/wordlearning/app/lib/Parse.py
@@ -19,7 +19,14 @@ from app.lib.Common import Common
 class Parse:
     @staticmethod
     def getHtml(url):
-        htmlfp = urllib.request.urlopen(url)
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+            }
+        )
+        htmlfp = urllib.request.urlopen(req)
         html = htmlfp.read().decode('utf-8', 'replace')
         htmlfp.close()
 
